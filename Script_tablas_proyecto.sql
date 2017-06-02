7182cebb374ab8ad0b4dc310dbaabc3941e74017
Fermach <f.manu_97@hotmail.com>
2017-06-02 16:03:17 +0200

    subida del proyeco vacio

---
+++ Script_tablas_proyecto.sql
@@ -0,0 +1,35 @@
+PRAGMA foreign_keys = ON;
+
+
+DROP TABLE IF EXISTS jugadores;
+CREATE TABLE jugadores(
+        login TEXT NOT NULL,
+        nombre TEXT,
+        apellidos TEXT,
+        nivel TEXT,
+        nacionalidad TEXT,
+PRIMARY KEY(login)
+);
+
+DROP TABLE IF EXISTS personajes;
+CREATE TABLE personajes (
+        nombre TEXT NOT NULL,
+        login TEXT NOT NULL,
+        tipo TEXT,
+        raza TEXT,
+        sexo TEXT,
+PRIMARY KEY(nombre,login),
+FOREIGN KEY(login) REFERENCES jugadores(login) ON UPDATE CASCADE ON DELETE CASCADE 
+);
+
+
+DROP TABLE IF EXIST historial;
+CREATE TABLE historial(
+        login TEXT NOT NULL,
+        nombre TEXT,
+        apellidos TEXT,
+        nivel TEXT,
+        nacionalidad TEXT,
+        fecha_baja TEXT,
+PRIMARY KEY(login)
+);
